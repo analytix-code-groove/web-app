4bc8249bcfc1abaedc5711a1c53fc0961187367f
Rodrigo Labarile <labarilerodrigo@gmail.com>
2025-08-15 11:52:30 -0300

    refactor: move rss endpoint to api folder

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/rss.xml',
+        destination: '/api/rss.xml',
+      },
+    ]
+  },
 }
 
 export default nextConfig
